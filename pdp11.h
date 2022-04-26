f74dc8d6fff4129cfd9e07003f4fd24aafac911f
teserk <eserkepov.t@phystech.edu>
2022-04-27 00:46:10 +0300

    сделал флаги (кроме V), arr тесты

--- pdp11.h
+++ pdp11.h
@@ -5,10 +5,12 @@
 #define LEN_SS 6
 #define LEN_NN 6
 #define LEN_R 3
+#define LEN_XX 8
 #define HAS_SS (1<<1)
 #define HAS_DD (1)
 #define HAS_R (1<<3)
 #define HAS_NN (1<<2)
+#define HAS_XX (1<<4)
 #define POSITION_B 15
 #define LEN_BYTE 8
 #define LEN_WORD 16
@@ -40,6 +42,29 @@ void do_add();
 void do_nothing();
 void do_sob();
 void do_clr();
+
+void do_ccc();
+void do_scc();
+
+void do_tst();
+void do_cmp();
+
+void do_cln();
+void do_clz();
+void do_clv();
+void do_clc();
+
+void do_sen();
+void do_sez();
+void do_sev();
+void do_sec();
+
+void set_C(int val);
+void set_NZ(word val);
+
+void do_br();
+void do_beq();
+
 void run();
 void trace(char * format, ...);
 byte b_read (adr a);
